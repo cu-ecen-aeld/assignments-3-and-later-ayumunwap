6cf7605a5e95964f7c519e1d56f869820d322224
ayamatani <ayamatani@Ubuntu.myguest.virtualbox.org>
2025-02-22 18:33:43 +0900

    assignment-1 implementation

---
+++ finder-app/finder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -u
+
+# the first argument is a path to a directory on the filesystem
+filesdir=$1
+# the second argument is a text string which will be searched within these files
+searchstr=$2
+
+if [ $# -lt 2 ]; then
+	echo "One or more of the specified arguments are not specified"
+	exit 1
+else
+	if [ ! -d $filesdir ]; then
+		echo "The second argument does not represent a directory on the filesystem"
+		exit 1
+	else
+		# X is the number of files in the directory and all subdirectories
+		X=`find $filesdir -type f | wc -l`
+		# Y is the number of matching lines found in respective files
+		Y=`grep -r $searchstr $filesdir | wc -l`	
+		echo "The number of files are ${X} and the number of matching lines are ${Y}"
+	fi
+fi
